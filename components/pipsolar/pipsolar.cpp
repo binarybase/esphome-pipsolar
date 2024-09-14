93620d65e1b2dd5511cc8532d394cdd78b4c8709
binarybase <87497154+binarybase@users.noreply.github.com>
2024-09-14 15:50:33 +0200

    fix: pipsolar CRC

--- components/pipsolar/pipsolar.cpp
+++ components/pipsolar/pipsolar.cpp
@@ -894,7 +894,7 @@ uint8_t Pipsolar::check_incoming_length_(uint8_t length) {
 
 uint8_t Pipsolar::check_incoming_crc_() {
   uint16_t crc16;
-  crc16 = cal_crc_half_(read_buffer_, read_pos_ - 3);
+  crc16 = this->pipsolar_crc(read_buffer_, read_pos_ - 3);
   ESP_LOGD(TAG, "checking crc on incoming message");
   if (((uint8_t)((crc16) >> 8)) == read_buffer_[read_pos_ - 3] &&
       ((uint8_t)((crc16) &0xff)) == read_buffer_[read_pos_ - 2]) {
@@ -925,7 +925,7 @@ uint8_t Pipsolar::send_next_command_() {
     this->command_start_millis_ = millis();
     this->empty_uart_buffer_();
     this->read_pos_ = 0;
-    crc16 = cal_crc_half_(byte_command, length);
+    crc16 = this->pipsolar_crc(byte_command, length);
     this->write_str(command);
     // checksum
     this->write(((uint8_t)((crc16) >> 8)));   // highbyte
@@ -958,7 +958,7 @@ void Pipsolar::send_next_poll_() {
   this->command_start_millis_ = millis();
   this->empty_uart_buffer_();
   this->read_pos_ = 0;
-  crc16 = cal_crc_half_(this->used_polling_commands_[this->last_polling_command_].command,
+  crc16 = this->pipsolar_crc(this->used_polling_commands_[this->last_polling_command_].command,
                         this->used_polling_commands_[this->last_polling_command_].length);
   this->write_array(this->used_polling_commands_[this->last_polling_command_].command,
                     this->used_polling_commands_[this->last_polling_command_].length);
